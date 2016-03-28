53908fc18370e5a36ba47faaf042c7ecb0b39c72
Al Jachimiak <aljachimiak@gmail.com>
2016-03-28 17:09:22 -0400

    print style and remove mongo

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
-var replaceStr = '<style> .skill-info{ margin-left: 1em; }</style><link rel="icon" href="apple-touch-icon.png"></head>'
- 
+
+var replaceStr = '<style> .skill-info{ margin-left: 1em; } @media print { .profile-card .icon { display: block !important;} .social-lnks{ display:none !importnt; } } </style>';
+replaceStr += '<link rel="icon" href="apple-touch-icon.png"></head>';
+
 gulp.task('insert-to-head', function(){
   gulp.src(['./resume.html', './index.html'])
     .pipe(replace('</head>', replaceStr))
