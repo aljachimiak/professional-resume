960c63757f8873eefd5c657c800a8ae08632555c
Al Jachimiak <aljachimiak@gmail.com>
2016-03-29 14:38:33 -0400

    print css tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 
-var replaceStr = '<style> .skill-info{ margin-left: 1em; } @media print { .profile-card .icon { display: block !important;} .social-lnks{ display:none !importnt; } } </style>';
+var replaceStr = '<style> .skill-info{ margin-left: 1em; }'
+replaceStr += ' @media print {';
+replaceStr += ' .profile-card .icon { display: block !important;}';
+replaceStr += ' .social-links{ display:none !important} ';
+replaceStr += ' .card-nested { page-break-inside: avoid; }';
+replaceStr += ' .card { min-height: 100px !important; }';
+replaceStr += ' .profile-pic img { height: 60px; width:60px;}';
+replaceStr += ' .background-card h4:first-of-type {display:none;}';
+replaceStr += ' .background-card .detail h4:first-of-type {display:block;}';
+replaceStr += ' .background-card hr:first-of-type{display:none;}';
+replaceStr += ' #skills .content li, #interests .content li, #education .content li { width: 48%; float: left; display: inline-block;}';
+replaceStr += ' #references { page-break-before: always; }';
+replaceStr += ' } </style>';
 replaceStr += '<link rel="icon" href="apple-touch-icon.png"></head>';
 
 gulp.task('insert-to-head', function(){
